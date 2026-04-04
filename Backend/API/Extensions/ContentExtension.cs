2b8c925001838084ddfc4850507eab1807f1c26d
Zynres <198392943+Woolction@users.noreply.github.com>
2026-04-04 13:30:24 +0300

    Deleted Extensions
    Deleted User And Domain Extensions

--- Backend/API/Extensions/ContentExtension.cs
+++ Backend/API/Extensions/ContentExtension.cs
@@ -5,17 +5,6 @@ namespace Backend.API.Extensions;
 
 public static class ContentExtension
 {
-    public static ContentRecoDto GetContentRecoDto(this Content content)
-    {
-        return new ContentRecoDto(
-            content.Id, content.DomainId, content.CreatorId,
-            content.Title, content.Slug, content.Description,
-            content.CreatedDate, content.ContentType.ToString(), Random.Shared.NextDouble(),
-            content.VideoMeta?.DurationSeconds, content.ContentUrl, content.PrewievPhotoUrl,
-            content.Savers.Count, content.Likers.Count, content.Comments.Count,
-            content.DizLikers.Count, content.ViewsCount);
-    }
-
     public static ContentResponseDto GetContentResponseDto(this Content content)
     {
         return new ContentResponseDto(
