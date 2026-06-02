55284b287e56f44cc5c6613730aa308941a8227b
Zynres <killp8918@gmail.com>
2026-06-02 19:41:51 +0300

    feat: add dependencies in di and add mediatR service

--- src/Workers/Program.cs
+++ src/Workers/Program.cs
@@ -1,3 +1,7 @@
+using Application;
+using Application.Interfaces.Services;
+using Workers.Consumers;
+
 namespace Workers;
 
 public class Program
@@ -6,6 +10,21 @@ public class Program
     {
         var builder = Host.CreateApplicationBuilder(args);
         builder.Services.AddHostedService<Worker>();
+
+        // Logging
+        builder.Logging.ClearProviders();
+        builder.Logging.AddConsole();
+        builder.Logging.AddDebug();
+        builder.Logging.SetMinimumLevel(LogLevel.Information);
+
+        // MediatR
+        builder.Services.AddMediatR(cf =>
+            cf.RegisterServicesFromAssembly(typeof(AppMaker).Assembly));
+
+        // Consumers
+        builder.Services.AddTransient<IConsumer, SearchIndexUpsertingConsumer>();
+        builder.Services.AddTransient<IConsumer, VideoUploadingConsumer>();
+        builder.Services.AddTransient<IConsumer, IconUploadingConsumer>();
 
         var host = builder.Build();
         host.Run();
