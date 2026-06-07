6da5ca7a74f42be0d5a9d05a05cf482f9c534dda
Zynres <killp8918@gmail.com>
2026-06-07 20:37:31 +0300

    fix: add worker dependencies

--- src/Workers/Program.cs
+++ src/Workers/Program.cs
@@ -1,8 +1,15 @@
 using Application.Interfaces.Services;
 using Application.Interfaces.Db;
+using Application.Features.Rows;
 using Infrastructure.Connector;
+using Infrastructure.Services;
+using Infrastructure.Context;
 using Workers.Consumers;
 using Application;
+using Infrastructure.Repositories;
+using Application.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Elastic.Clients.Elasticsearch;
 
 namespace Workers;
 
@@ -12,6 +19,25 @@ public class Program
     {
         var builder = Host.CreateApplicationBuilder(args);
         builder.Services.AddHostedService<Worker>();
+
+        // Services:
+        builder.Services.AddSingleton<RabbitConnectorFactory>();
+
+        builder.Services.AddSingleton<IRabbitMqConnector>(provider =>
+            provider.GetRequiredService<RabbitConnectorFactory>());
+
+        builder.Services.AddDbContext<EndlessContext>(context => context.UseNpgsql(
+            builder.Configuration.GetConnectionString("DB")));
+        builder.Services.AddScoped<IAppDbContext>(provider =>
+            provider.GetRequiredService<EndlessContext>());
+
+        builder.Services.AddSingleton(sp =>
+        {
+            var settings = new ElasticsearchClientSettings(new Uri("http://search:9200"))
+                .DefaultIndex("users");
+
+            return new ElasticsearchClient(settings);
+        });
 
         // Logging
         builder.Logging.ClearProviders();
@@ -23,11 +49,16 @@ public class Program
         builder.Services.AddMediatR(cf =>
             cf.RegisterServicesFromAssembly(typeof(AppMaker).Assembly));
 
-        // RabbitMQ
-        builder.Services.AddSingleton<RabbitConnectorFactory>();
+        // Depends
+        builder.Services.AddSingleton<IFfmpegService, FfmpegService>();
+        builder.Services.AddSingleton<IStorage, R2Service>();
 
-        builder.Services.AddSingleton<IRabbitMqConnector>(provider =>
-            provider.GetRequiredService<RabbitConnectorFactory>());
+        builder.Services.AddScoped<IChannelRepository, ChannelRepository>();
+        builder.Services.AddScoped<IContentRepository, ContentRepository>();
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+        // Publishers
+        builder.Services.AddSingleton<SearchIndexUpsertPublisher>();
 
         // Consumers
         builder.Services.AddTransient<IConsumer, SearchIndexUpsertingConsumer>();
